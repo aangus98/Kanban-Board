9adfac4174a033474c22fb00ae049c016521a205
Andrew <mrangus298@gmail.com>
2025-01-04 10:39:05 -0600

    Fixed up the todo's in auth.ts, authAPI.tsx

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,42 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { redirect } from 'react-router-dom';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    return false;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    if (!token) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    redirect('/');
+    
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    redirect('/login');
   }
 }
 
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,24 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.sendStatus(401);
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      if (typeof user !== 'string' && user !== undefined) {
+        req.user = user as JwtPayload;
+      } else {
+        return res.sendStatus(403);
+      }
+      req.user = user as JwtPayload;
+      next();
+      return;
+    });
+  }
+  return;
 };
