830743454c9e27269f2d1b656addb0112cf5e4ae
Andrew <mrangus298@gmail.com>
2025-01-04 10:54:25 -0600

    Updated auth-routes and .env

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,18 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { username } });
+  if (!user) {
+    return res.status(400).json({ error: 'Invalid username or password' });
+  } else {
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET as string);
+      return res.json({ accessToken });
+    } else {
+      return res.status(400).json({ error: 'Invalid username or password' });
+    }
+  }
 };
 
 const router = Router();
